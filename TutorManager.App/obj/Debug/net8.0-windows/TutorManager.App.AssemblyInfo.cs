0de6387957490ac2f50d4fa591378ffc6342a9e0
DELL <DELL@DESKTOP-1UDEMKU>
2026-05-02 19:13:15 -0400

    added first build

--- TutorManager.App/obj/Debug/net8.0-windows/TutorManager.App.AssemblyInfo.cs
+++ TutorManager.App/obj/Debug/net8.0-windows/TutorManager.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TutorManager.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97ce6875786854d17d30b5b0e6f96add6ea533c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00980071064bf7d9eb8db258bf7dc9e4a4330178")]
 [assembly: System.Reflection.AssemblyProductAttribute("TutorManager.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TutorManager.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
